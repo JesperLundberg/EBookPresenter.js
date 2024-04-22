bc78424bb23d2cf4eb1e095a55f94a054bdf5c3a
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-04-22 12:40:28 +0200

    Add mock-fs and set up mocked filesystem

--- src/services/__test__/filesystem.test.js
+++ src/services/__test__/filesystem.test.js
@@ -1,46 +1,38 @@
-import fs from "fs";
-import { jest } from "@jest/globals";
 import fileSystem from "../filesystem.js";
+import mock from "mock-fs";
 
-// jest.mock("fs");
-
-// jest.mock("fs", () => ({
-//   readdirSync: jest.fn(),
-// }));
-
-jest.unstable_mockModule("fs", () => ({
-  readdirSync: jest
-    .fn()
-    .mockReturnValue([
-      "src/tests/ebooks/ebook1.epub",
-      "src/tests/ebooks/ebook2.epub",
-    ]),
-}));
+mock({
+  "path/to/fake/dir": {
+    "some-file.epub": "file content here",
+    "other-dir": {
+      "some-other.epub": "other file content here",
+      "some-third.epub": "other file content here",
+    },
+    "empty-dir": {},
+  },
+  "path/to/some.png": Buffer.from([8, 6, 7, 5, 3, 0, 9]),
+});
 
 describe("getAllEBooks", () => {
   it("should return an array of all ebooks in the directory and subdirectories", async () => {
-    const readdirSync = jest.spyOn(fs, "readdirSync");
-    // let fs = jest.mock("fs");
-    //
-    // fs.readdirSync = jest
-    //   .mock()
-    //   .fn()
-    //   .mockReturnValue([
-    //     "src/tests/ebooks/ebook1.epub",
-    //     "src/tests/ebooks/ebook2.epub",
-    //   ]);
-    // Arrange
-
-    // console.log("fs", fs.readdirSync());
-
     // Act
-    const ebooks = await fileSystem.getAllEBooks();
-    expect(fs.readdirSync).toHaveBeenCalledTimes(2);
+    const ebooks = await fileSystem.getAllEBooks("path/");
 
     // Assert
-    // expect(ebooks).toEqual([
-    //   "src/tests/ebooks/ebook1.epub",
-    //   "src/tests/ebooks/ebook2.epub",
-    // ]);
+    expect(ebooks).toEqual([
+      "path/to/fake/dir/other-dir/some-other.epub",
+      "path/to/fake/dir/other-dir/some-third.epub",
+      "path/to/fake/dir/some-file.epub",
+    ]);
+  });
+});
+
+describe("getAllEBooks", () => {
+  it("should return empty array if no ebooks are found", async () => {
+    // Act
+    const ebooks = await fileSystem.getAllEBooks("path/to/fake/dir/empty-dir");
+
+    // Assert
+    expect(ebooks).toEqual([]);
   });
 });
