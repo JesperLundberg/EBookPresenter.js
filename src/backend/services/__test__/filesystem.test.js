331768f8ee404b48c95698f3e85001122be399cd
Mikael Lundin <mikael@incaps.se>
2024-04-26 15:29:47 +0200

    Testing testing, 1, 2, 1, 2

--- src/backend/services/__test__/filesystem.test.js
+++ src/backend/services/__test__/filesystem.test.js
@@ -1,50 +1,71 @@
-import fileSystem from "../filesystem.js";
-import mock from "mock-fs";
+var sut = require("../filesystem");
+
+// mocking fs module
+var fs = require("fs");
+jest.mock("fs");
 
 describe("getAllEBooks", () => {
-  it("should return an array of all ebooks in the directory and subdirectories", async () => {
-    // Arrange
-    mock({
-      "path/to/fake/dir": {
-        "some-file.epub": "file content here",
-        "other-dir": {
-          "some-other.epub": "other file content here",
-          "some-third.epub": "other file content here",
-        },
-        "empty-dir": {},
+  //
+  // common mocks
+  //
+
+  // all of our directories ends with dir in the name
+  fs.statSync.mockImplementation((path) => {
+    return {
+      isDirectory: () => {
+        return path.endsWith("dir");
       },
-      "path/to/some.png": Buffer.from([8, 6, 7, 5, 3, 0, 9]),
+    };
+  });
+
+  // yeah, in these tests all files exists
+  fs.existsSync.mockImplementation(() => {
+    return true;
+  });
+
+  it("should return an array of all ebooks in the directory and subdirectories", async () => {
+    // arrange
+
+    // mock file structure
+    fs.readdirSync.mockImplementation((dirPath) => {
+      if (dirPath.includes("other-dir")) {
+        return ["some-other.epub", "some-third.epub"];
+      }
+      if (dirPath.includes("empty-dir")) {
+        return [];
+      }
+      return ["some-file.epub", "other-dir", "empty-dir", "nsfw.png"];
     });
 
     // Act
-    const ebooks = await fileSystem.getAllEBooks("path/");
+    const ebooks = await sut.getAllEBooks("path/");
 
     // Assert
     expect(ebooks).toEqual([
-      "path/to/fake/dir/other-dir/some-other.epub",
-      "path/to/fake/dir/other-dir/some-third.epub",
-      "path/to/fake/dir/some-file.epub",
+      "path/some-file.epub",
+      "path/other-dir/some-other.epub",
+      "path/other-dir/some-third.epub",
     ]);
   });
 });
 
 describe("getAllEBooks", () => {
   it("should return empty array if no ebooks are found", async () => {
-    // Arrange
-    mock({
-      "path/to/fake/dir": {
-        "file-here.bin": "file content here",
-        "other-dir": {
-          "some-other.som": "other file content here",
-          "some-third.oth": "other file content here",
-        },
-        "empty-dir": {},
-      },
-      "path/to/some.png": Buffer.from([8, 6, 7, 5, 3, 0, 9]),
+    // arrange
+
+    // These are not the files you are looking for
+    fs.readdirSync.mockImplementation((dirPath) => {
+      if (dirPath.includes("other-dir")) {
+        return ["some-other.som", "some-third.oth"];
+      }
+      if (dirPath.includes("empty-dir")) {
+        return [];
+      }
+      return ["some.png"];
     });
 
     // Act
-    const ebooks = await fileSystem.getAllEBooks("path/");
+    const ebooks = await sut.getAllEBooks("path/");
 
     // Assert
     expect(ebooks).toEqual([]);
