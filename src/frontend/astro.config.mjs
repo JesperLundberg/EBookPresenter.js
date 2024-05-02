8f3e8d567230e7635458a8c0584ed8f1ab9cb8a6
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-05-02 12:32:07 +0200

    add initial styling

--- src/frontend/astro.config.mjs
+++ src/frontend/astro.config.mjs
@@ -1,5 +1,6 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,4 +13,5 @@ export default defineConfig({
       noExternal: ["path-to-regexp"],
     },
   },
+  integrations: [tailwind()],
 });
