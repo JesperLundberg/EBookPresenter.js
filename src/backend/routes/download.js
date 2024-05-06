67f8b00c0109d23b92cddcf1985b837a1b5d4481
Jesper Lundberg <lundberg.jesper@gmail.com>
2024-05-06 10:31:36 +0200

    Make download into a link instead

--- src/backend/routes/download.js
+++ src/backend/routes/download.js
@@ -3,11 +3,9 @@ const express = require("express");
 const routes = express.Router();
 
 routes.get("/download", async (req, res, next) => {
-  console.log("Download request received");
-  console.log("req.query", req.query.path.trim());
+  // Strip any leading/trailing whitespace from the path read from the query string
   try {
     const fullFilePath = req.query.path.trim();
-    console.log("fullFilePath", fullFilePath);
     res.header("Content-Type", "application/epub+zip");
     res.download(fullFilePath);
   } catch (error) {
